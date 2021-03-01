999da174b2a9e4dd4f768d6b86c509486af32fab
Alejandro Valenzuela <alevagre7@gmail.com>
2021-03-01 02:42:38 +0100

    More additions to the Tap Dance feature detailed on the last commit.

--- users/Alevagre7/alevagre7.h
+++ users/Alevagre7/alevagre7.h
@@ -22,9 +22,11 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 // Mod-Taps
 //  Layers
-#define SPLO LT(_LOWER, KC_SPC)
-#define BSRA LT(_RAISE, KC_BSPC)
+#define KC_SPLO LT(_LOWER, KC_SPC)
+#define KC_BSRA LT(_RAISE, KC_BSPC)
 //  Holders
 #define KC_ESCC LCTL_T(KC_ESC)
 #define KC_LSF7 LSFT_T(KC_F7)
 #define KC_LCF1 LCTL_T(KC_F1)
+//  Tap Dances
+#define KC_LSCL TD(TD_LSFT_CAPS)
